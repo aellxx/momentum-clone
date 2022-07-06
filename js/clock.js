fc4558ffe4a898af346c7415da5ba8fa0cb44633
Ael <bluebayband@gmail.com>
2022-07-06 13:31:18 -0400

    added clock

---
+++ js/clock.js
@@ -0,0 +1,18 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+    // recreate date object every second
+    const date = new Date();
+
+    // pad the numbers if applicable
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+// show time without 1 second delay
+getClock();
+// update time every second 
+setInterval(getClock, 1000);
