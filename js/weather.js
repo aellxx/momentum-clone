c1a744f0806be9553c89f44e1b09f5cd603e397d
Ael <bluebayband@gmail.com>
2022-07-09 10:45:06 -0400

    fix

--- js/weather.js
+++ js/weather.js
@@ -3,19 +3,8 @@ const API_KEY = "add api key here";
 function onGeoOk(position) {
     // get latitude and longitude from automatic param position
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
-    // fetch url
-    fetch(url)
-        .then(response => {response.json()}) 
-        .then(data => {
-            const weather = document.querySelector("#weather span:first-child");
-            const city = document.querySelector("#weather span:last-child")
-
-            city.innerText = data.name;
-            weather.innerText = data.weather[0].main;
-        });
+    const long = position.coords.longitude;
+    console.log(`You live in (${lat}, ${long})`);
 }
 
 // function to run if not able to get location
