7cfa3a04535e84ef715eeb01fcac1a87e45664c1
Ael <bluebayband@gmail.com>
2022-07-09 10:31:07 -0400

    add weather features

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,21 @@
+const API_KEY = "78f91203b9ea6e5474fcfb2af86c432a";
+
 function onGeoOk(position) {
     // get latitude and longitude from automatic param position
     const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-    console.log(`You live in (${lat}, ${long})`);
+    const lng = position.coords.longitude;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    // fetch url
+    fetch(url)
+        .then(response => {response.json()}) 
+        .then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child")
+
+            city.innerText = data.name;
+            weather.innerText = data.weather[0].main;
+        });
 }
 
 // function to run if not able to get location
